27b9625b15d4579eee1737ee7e1043e1d6172168
rdean3107 <162683228+rdean3107@users.noreply.github.com>
2024-07-05 08:08:46 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,12 +5,12 @@ const fs = require("fs");
 const { Classic } = require("musicard");
 const { prefix } = require('./config.json');
 const nodes = [
-    {
-        host: "lava-v4.ajieblogs.eu.org",
-        password: "https://dsc.gg/ajidevserver",
-        port: 443,
-        secure: true
-    },
+     {
+    host: "lavalink.oryzen.xyz",
+    port: 80, 
+    password: "oryzen.xyz", 
+     secure: false
+  },
 ];
 
 
@@ -304,7 +304,7 @@ client.riffy.on("queueEnd", async (player) => {
           iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6641e8a8&is=66409728&hm=149efc9db2a92eb90c70f0a6fb15618a5b912b528f6b1dcf1b517c77a72a733a&',
           url: 'https://discord.gg/xQF9f9yUEM'
         })
-          .setDescription('**Bye Bye!, No more songs to play... hate yêu bạn **');
+          .setDescription('**Bye Bye!, No more songs to play...**');
           channel.send({ embeds: [embed] });
       
     }
